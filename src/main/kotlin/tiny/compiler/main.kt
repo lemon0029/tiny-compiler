00ccc3b96f66471adb4bb7dcfa39c8462162c664
imoyb <imoyb@qq.com>
2023-01-08 14:56:15 +0800

    sample

--- src/main/kotlin/tiny/compiler/main.kt
+++ src/main/kotlin/tiny/compiler/main.kt
@@ -4,10 +4,24 @@ import tiny.compiler.generator.codeGenerator
 import tiny.compiler.parser.parse
 import tiny.compiler.parser.tokenize
 import tiny.compiler.traverser.transform
+import java.nio.file.Files
+import kotlin.io.path.toPath
 
 fun compile(input: String): String {
     val tokens = tokenize(input)
     val ast = parse(tokens)
     val transformed = transform(ast)
     return codeGenerator(transformed)
+}
+
+fun main(args: Array<String>) {
+    val sourceCodeFileName = args[0]
+
+    val path = Thread.currentThread().contextClassLoader.getResource(sourceCodeFileName)?.toURI()?.toPath() ?: return
+    val input = Files.readString(path)
+    println("[$sourceCodeFileName]\n$input\n")
+
+
+    val output = compile(input)
+    println("[${sourceCodeFileName.removeSuffix(".lisp") + ".c"}]\n$output\n")
 }
---
+++ src/main/resources/hello.lisp
@@ -0,0 +1,5 @@
+(add 1123 2)
+(add 39 4)
+(add 51 (subtract 6 7))
+(subtract 61 799)
+(subtract 116 (add 12 (add 94382 (add 63 (add 40 (subtract 45 609))))))
